0e502693c396acf50c2b339f90757eb53ccba59c
Tkkg1994 <luca.grifo@outlook.com>
2016-05-17 15:16:38 +0200

    stock G935FXXU1APDN release, deodexed, rooted and busybox support

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:38914048:69ea9347f441cf5c56b8d61284843a5f24b551e8; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:36646912:8c76f4f7b68ed10f4ac6d7c12954ee1db1953c9c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 69ea9347f441cf5c56b8d61284843a5f24b551e8 38914048 8c76f4f7b68ed10f4ac6d7c12954ee1db1953c9c:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:38922240:db61a615a181a055c931e79afe2473efbf089983; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:36644864:b5b348e625a52b73add98dc83b524cf6e317d3eb EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY db61a615a181a055c931e79afe2473efbf089983 38922240 b5b348e625a52b73add98dc83b524cf6e317d3eb:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
