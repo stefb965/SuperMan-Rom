ad81effd8ed0b81e6a9a97dd2014a8f229df8607
Tkkg1994 <luca.grifo@outlook.com>
2016-06-09 13:37:10 +0200

    G935FXXU1APF2 release

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:38998016:2bd65ad7d60eaf8abbbfee198454ff29915cdaad; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:36720640:f02fe40ac935ac1bd0e18443546e57cc93a6fd08 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 2bd65ad7d60eaf8abbbfee198454ff29915cdaad 38998016 f02fe40ac935ac1bd0e18443546e57cc93a6fd08:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39006208:824dd4f44eec9cb6dae40c03fd6095d0d05dce39; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:36730880:eda26aa7a34eaac4a8b17ec8df18fe34f7e998eb EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 824dd4f44eec9cb6dae40c03fd6095d0d05dce39 39006208 eda26aa7a34eaac4a8b17ec8df18fe34f7e998eb:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
