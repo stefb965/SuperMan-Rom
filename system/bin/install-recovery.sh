b713d6da58141eb5b182874f0e719ac9d2295517
Tkkg1994 <lg@linux.com>
2016-07-08 14:32:19 +0200

    G935FXXU1APFG release with BTU csc included (because VD2 has to many bloat)

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39006208:824dd4f44eec9cb6dae40c03fd6095d0d05dce39; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:36730880:eda26aa7a34eaac4a8b17ec8df18fe34f7e998eb EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 824dd4f44eec9cb6dae40c03fd6095d0d05dce39 39006208 eda26aa7a34eaac4a8b17ec8df18fe34f7e998eb:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39004160:45dd0899f1805ee820354a3cd0c35d08e2991ff3; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:36730880:7beff2c01ed3231807797528014381bf93a814cf EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 45dd0899f1805ee820354a3cd0c35d08e2991ff3 39004160 7beff2c01ed3231807797528014381bf93a814cf:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
