1466aceb1870f3684d9667ddc5f6bd17d288e6df
Tkkg1994 <lg@linux.com>
2016-07-15 14:07:24 +0200

    APG5 release

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39004160:45dd0899f1805ee820354a3cd0c35d08e2991ff3; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:36730880:7beff2c01ed3231807797528014381bf93a814cf EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 45dd0899f1805ee820354a3cd0c35d08e2991ff3 39004160 7beff2c01ed3231807797528014381bf93a814cf:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39004160:ec2cc27b698ffce8581c931961f212b75b8ac288; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:36730880:8fd7b7b4a35c9bfac572c4b173d537b8b6f43247 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY ec2cc27b698ffce8581c931961f212b75b8ac288 39004160 8fd7b7b4a35c9bfac572c4b173d537b8b6f43247:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
