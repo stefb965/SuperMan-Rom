8487afb8ed627d0c99088d8f2554d8c669f4ee4e
Tkkg1994 <lg@linux.com>
2016-08-07 15:19:18 +0200

    APGO deodexed base

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39004160:ec2cc27b698ffce8581c931961f212b75b8ac288; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:36730880:8fd7b7b4a35c9bfac572c4b173d537b8b6f43247 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY ec2cc27b698ffce8581c931961f212b75b8ac288 39004160 8fd7b7b4a35c9bfac572c4b173d537b8b6f43247:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39004160:fd5569d0b4a2528136a46a4871d1bc5c76ab8862; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:36730880:53a567b55aa6ec30bf490e0fa32c5d08f45932fa EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY fd5569d0b4a2528136a46a4871d1bc5c76ab8862 39004160 53a567b55aa6ec30bf490e0fa32c5d08f45932fa:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
