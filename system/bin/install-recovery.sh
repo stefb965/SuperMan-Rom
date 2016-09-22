c9ab7544b36b09aa676eaef0d7dc202856a16b7b
Tkkg1994 <lg@linux.com>
2016-09-22 14:17:17 +0200

    BPIF deodexed base

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39794688:62b46224965bc25c0724365af982876fc0ee0707; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37523456:46c8ae94bf13218e1205f034da800ff048b4290d EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 62b46224965bc25c0724365af982876fc0ee0707 39794688 46c8ae94bf13218e1205f034da800ff048b4290d:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39794688:d4bcc4808ac7d926482a4a44589bef726fcecebf; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37523456:6957c1af149013fb6bfd5a2ebf92e0a9f7dfc4b3 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY d4bcc4808ac7d926482a4a44589bef726fcecebf 39794688 6957c1af149013fb6bfd5a2ebf92e0a9f7dfc4b3:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
