ab15fe95868299e1f0ce0d5213a8743deaeb9672
Luca Grifo <lg@linux.com>
2016-12-13 01:25:50 +0100

    small cleanup and adding two missing files from META-INF folder

---
+++ META-INF/com/google/android/aroma/checkconfig/checkconfig.sh
@@ -0,0 +1,9 @@
+#!/sbin/sh
+# Written by Tkkg1994
+
+mount /dev/block/platform/155a0000.ufs/by-name/USERDATA /data
+
+if [ -e /data/media/0/SuperMan/aroma/wipe.prop ];then
+	echo "restore=yes" > /tmp/aroma/restore.prop
+fi
+
