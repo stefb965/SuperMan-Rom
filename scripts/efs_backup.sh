681983d41ad3cdf1f8893f7fc152adc274c00b30
Luca Grifo <lg@linux.com>
2016-12-13 17:35:43 +0100

    Little bit tired yesterday lol

--- scripts/efs_backup.sh
+++ scripts/efs_backup.sh
@@ -1,6 +1,7 @@
 #!/sbin/sh
 # Written by Tkkg1994
 
+mount /dev/block/platform/155a0000.ufs/by-name/SYSTEM /system
 mount /dev/block/platform/155a0000.ufs/by-name/USERDATA /data
 mount /dev/block/platform/155a0000.ufs/by-name/EFS /efs
 
