e80c8a735f758e19b851ca4ceb4c9d332a248886
Luca Grifo <lg@linux.com>
2016-12-13 02:06:42 +0100

    final clean ups

--- META-INF/com/google/android/aroma/checkconfig/checkconfig.sh
+++ META-INF/com/google/android/aroma/checkconfig/checkconfig.sh
@@ -3,7 +3,7 @@
 
 mount /dev/block/platform/155a0000.ufs/by-name/USERDATA /data
 
-if [ -e /data/media/0/SuperMan/aroma/wipe.prop ];then
+if [ -e /data/media/0/SuperMan/aroma/wipe.prop ]; then
 	echo "restore=yes" > /tmp/aroma/restore.prop
 fi
 
--- scripts/model.sh
+++ scripts/model.sh
@@ -3,20 +3,22 @@
 
 getprop ro.boot.bootloader >> /tmp/BLmodel
 
-mount /dev/block/platform/155a0000.ufs/by-name/USERDATA /data
+mount /dev/block/platform/155a0000.ufs/by-name/SYSTEM /system
 
 # Change CSC to right model
-if grep -q G935 /tmp/BLmodel;
-then
-	sed -i -- 's/G930/G935/g' /system/CSCVersion.txt
-	sed -i -- 's/G930/G935/g' /system/SW_Configuration.xml
-else if grep -q G930 /tmp/BLmodel;
-then
-	sed -i -- 's/G935/G930/g' /system/CSCVersion.txt
-	sed -i -- 's/G935/G930/g' /system/SW_Configuration.xml
-else
-	echo "Not a supported model, keep csc config default!"
-fi
+if [ -e /system/CSCVersion.txt ]; then
+	if grep -q G935 /tmp/BLmodel;
+	then
+		sed -i -- 's/G930/G935/g' /system/CSCVersion.txt
+		sed -i -- 's/G930/G935/g' /system/SW_Configuration.xml
+	else if grep -q G930 /tmp/BLmodel;
+	then
+		sed -i -- 's/G935/G930/g' /system/CSCVersion.txt
+		sed -i -- 's/G935/G930/g' /system/SW_Configuration.xml
+	else
+		echo "Not a supported model, keep csc config default!"
+	fi
+	fi
 fi
 
 # Change build.prop to right model
@@ -26,18 +28,23 @@ then
 else if grep -q G930F /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G930F/g' /system/build.prop
+	sed -i -- 's/hero2lte/herolte/g' /system/build.prop	
 else if grep -q G930K /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G930K/g' /system/build.prop
+	sed -i -- 's/hero2lte/herolte/g' /system/build.prop
 else if grep -q G930L /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G930L/g' /system/build.prop
+	sed -i -- 's/hero2lte/herolte/g' /system/build.prop
 else if grep -q G930S /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G930S/g' /system/build.prop
+	sed -i -- 's/hero2lte/herolte/g' /system/build.prop
 else if grep -q G930W8 /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G930W8/g' /system/build.prop
+	sed -i -- 's/hero2lte/herolte/g' /system/build.prop
 else if grep -q G935K /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G935K/g' /system/build.prop
