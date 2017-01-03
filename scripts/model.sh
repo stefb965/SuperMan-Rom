efd861a14a4d37cebbdfcca53d933aaa9fac83ae
Luca Grifo <lg@linux.com>
2017-01-03 22:49:26 +0100

    Many changes, check full commit log Updated scripts Updated fonts selection Updated nougat fonts Updated host file Updated Samsung Music app Updated Magisk to V10.2 Updated SuperSU to SR2 Updated phh magisk to R2 Updated Magisk apk Updated Phh apk Updated all kernel with latest release Added recoveries Updated sqlite binary

--- scripts/model.sh
+++ scripts/model.sh
@@ -28,35 +28,44 @@ then
 else if grep -q G930F /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G930F/g' /system/build.prop
-	sed -i -- 's/hero2lte/herolte/g' /system/build.prop	
+	sed -i -- 's/hero2lte/herolte/g' /system/build.prop
+	echo "Changing to G930F"
 else if grep -q G930K /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G930K/g' /system/build.prop
 	sed -i -- 's/hero2lte/herolte/g' /system/build.prop
+	echo "Changing to G930K"
 else if grep -q G930L /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G930L/g' /system/build.prop
 	sed -i -- 's/hero2lte/herolte/g' /system/build.prop
+	echo "Changing to G930L"
 else if grep -q G930S /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G930S/g' /system/build.prop
 	sed -i -- 's/hero2lte/herolte/g' /system/build.prop
+	echo "Changing to G930S"
 else if grep -q G930W8 /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G930W8/g' /system/build.prop
 	sed -i -- 's/hero2lte/herolte/g' /system/build.prop
+	echo "Changing to G930W8"
 else if grep -q G935K /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G935K/g' /system/build.prop
+	echo "Changing to G935K"
 else if grep -q G935L /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G935L/g' /system/build.prop
+	echo "Changing to G935L"
 else if grep -q G935S /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G935S/g' /system/build.prop
+	echo "Changing to G935S"
 else if grep -q G935W8 /tmp/BLmodel;
 then
 	sed -i -- 's/G935F/G935W8/g' /system/build.prop
+	echo "Changing to G935W8"
 else
 	echo "Not a supported model, keep build.prop default!"
 fi
