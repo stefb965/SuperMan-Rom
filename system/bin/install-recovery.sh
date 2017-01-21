ce92d168eea9c5f8a8e2ebce495d3ecde018ffc9
Luca Grifo <lg@linux.com>
2017-01-21 13:49:19 +0100

    Full stock DPLT rom (deodexed), only META-INF folder is from superman

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:39854080:4406ddf5277ddc8dd78acb279fd15937e0dd6e31; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37578752:70f13df779c08a817e1bc9b956903731ff655355 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 4406ddf5277ddc8dd78acb279fd15937e0dd6e31 39854080 70f13df779c08a817e1bc9b956903731ff655355:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:46014464:2bd03e7164379ccfc98282f11252d6ca90fb6df6; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37853184:d48dcc88c66c6c1ebece1537a49cacc39d63c5e5 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 2bd03e7164379ccfc98282f11252d6ca90fb6df6 46014464 d48dcc88c66c6c1ebece1537a49cacc39d63c5e5:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
