2473406f6e1ab7ccf3c91ba5f88aad8036aff9df
Luca Grifo <lg@linux.com>
2017-01-23 14:30:53 +0100

    Update bin/lib/lib64/xbin/cameradata/fake-lib to DQAS release

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:46014464:2bd03e7164379ccfc98282f11252d6ca90fb6df6; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37853184:d48dcc88c66c6c1ebece1537a49cacc39d63c5e5 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 2bd03e7164379ccfc98282f11252d6ca90fb6df6 46014464 d48dcc88c66c6c1ebece1537a49cacc39d63c5e5:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:46014464:8aa8de79e4d150cb86e0cb24c0b4a2ad68656e5c; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37853184:df95ab2c9058e466fdb95b9f557479e7cb412d27 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 8aa8de79e4d150cb86e0cb24c0b4a2ad68656e5c 46014464 df95ab2c9058e466fdb95b9f557479e7cb412d27:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
