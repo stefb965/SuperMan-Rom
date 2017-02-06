855cff3f67d1f2ca2adfc7ba6787fbf23e006449
Luca Grifo <lg@linux.com>
2017-02-06 03:04:58 +0100

    Update base to DQAZ, all mods included fixed weather on lockscreen Added analog watches on lockscreen

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:46014464:8aa8de79e4d150cb86e0cb24c0b4a2ad68656e5c; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37853184:df95ab2c9058e466fdb95b9f557479e7cb412d27 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 8aa8de79e4d150cb86e0cb24c0b4a2ad68656e5c 46014464 df95ab2c9058e466fdb95b9f557479e7cb412d27:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:46014464:6af905254d2a9427d2873091b8294ac607e67eb1; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37853184:f8627dc7834d23dc24f73bd4a756914b905b2299 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 6af905254d2a9427d2873091b8294ac607e67eb1 46014464 f8627dc7834d23dc24f73bd4a756914b905b2299:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
