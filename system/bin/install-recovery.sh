46339fd8bf6b2e57e1368e2a8e36338efe089348
Luca Grifo <lg@linux.com>
2017-02-07 17:56:22 +0100

    rebase on DQB3 knox apps added as aroma option all other mods updated to latest base

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:46014464:6af905254d2a9427d2873091b8294ac607e67eb1; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37853184:f8627dc7834d23dc24f73bd4a756914b905b2299 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 6af905254d2a9427d2873091b8294ac607e67eb1 46014464 f8627dc7834d23dc24f73bd4a756914b905b2299:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:46014464:734126f2e9deed30b563ac07909bd55420d62d48; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37853184:665f5fea796702b1e7ef6882a6be239ceb6c7d8b EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 734126f2e9deed30b563ac07909bd55420d62d48 46014464 665f5fea796702b1e7ef6882a6be239ceb6c7d8b:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
