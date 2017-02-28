a33e2303b5f03c855fe2484ee95eae6572881337
Luca Grifo <lg@linux.com>
2017-02-28 22:45:29 +0100

    rebase to DQBO with all mods and everything updated

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:46014464:5f6492119885cb2635b9524acdd1d6205bd57171; then
-  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37853184:e9c1edd969ae72994cc89595c84b33f54a95e6e5 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 5f6492119885cb2635b9524acdd1d6205bd57171 46014464 e9c1edd969ae72994cc89595c84b33f54a95e6e5:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY:46014464:4e169ffa81d1e93b74a7de362f09e8dcce23aa7d; then
+  applypatch EMMC:/dev/block/platform/155a0000.ufs/by-name/BOOT:37853184:966fcdf17eed616b50f25f73b5e5d29708c1ef47 EMMC:/dev/block/platform/155a0000.ufs/by-name/RECOVERY 4e169ffa81d1e93b74a7de362f09e8dcce23aa7d 46014464 966fcdf17eed616b50f25f73b5e5d29708c1ef47:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
